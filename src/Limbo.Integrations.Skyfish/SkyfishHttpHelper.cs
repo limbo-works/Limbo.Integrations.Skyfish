a9d2fa6131e07a6c494c1d4078315b6aa38ae124
Anders Bjerner <abjerner@skybrud.dk>
2022-02-11 15:52:40 +0100

    Introduced new "SkyfishHttpHelper" class
    Normally I leave it up to the packages or projects using the integration package to "interpret" the underlying API, but for the sake of this example, this could also partially be handled by a new helper class.
    
    For one, the helper class contains the "GetVideoByMediaId" and "GetVideoByUniqueMediaId" methods. These don't really exist in the underlying API, so they are really just calling the "Search" method with the right parameters, delegating the work to the underlying parts of the package.

---
+++ src/Limbo.Integrations.Skyfish/SkyfishHttpHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Limbo.Integrations.Skyfish.Models.Media;
+using Limbo.Integrations.Skyfish.Options.Videos;
+using Limbo.Integrations.Skyfish.Responses.Search;
+
+namespace Limbo.Integrations.Skyfish {
+    
+    public class SkyfishHttpHelper {
+
+        #region Properties
+
+        public SkyfishHttpService Service { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public SkyfishHttpHelper(SkyfishHttpService service) {
+            Service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        #endregion
+
+        #region Member methods
+
+        public SkyfishMediaItem GetVideoByMediaId(int mediaId) {
+
+            // Search for the video in the via the Search API
+            SkyfishSearchResponse response = Service.Search.Search(new SkyfishSearchOptions {
+                MediaId = mediaId
+            });
+
+
+            return response.Body.Media.FirstOrDefault();
+
+        }
+
+        public SkyfishMediaItem GetVideoByUniqueMediaId(int uniqueMediaId) {
+
+            // Search for the video in the via the Search API
+            SkyfishSearchResponse response = Service.Search.Search(new SkyfishSearchOptions {
+                UniqueMediaId = uniqueMediaId
+            });
+
+
+            return response.Body.Media.FirstOrDefault();
+
+        }
+
+        #endregion
+
+    }
+
+}
