185a4f717cc5d689741203ca7b3dc18cf888875a
Anders Bjerner <abjerner@skybrud.dk>
2023-12-12 11:50:34 +0100

    IReadOnlyList FTW

--- src/Limbo.Integrations.Skyfish/Models/Search/SkyfishSearchResult.cs
+++ src/Limbo.Integrations.Skyfish/Models/Search/SkyfishSearchResult.cs
@@ -1,16 +1,17 @@
-﻿using Limbo.Integrations.Skyfish.Models.Media;
+﻿using System.Collections.Generic;
+using Limbo.Integrations.Skyfish.Models.Media;
 using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Json;
 using Skybrud.Essentials.Json.Extensions;
 
 namespace Limbo.Integrations.Skyfish.Models.Search {
-    
+
     public class SkyfishSearchResult : JsonObjectBase {
 
         public int Hits { get; }
 
-        public SkyfishMediaItem[] Media { get; }
-        
+        public IReadOnlyList<SkyfishMediaItem> Media { get; }
+
         private SkyfishSearchResult(JObject json) : base(json) {
             Hits = json.GetInt32("response.hits");
             Media = json.GetArrayItems("response.media", SkyfishMediaItem.Parse);
